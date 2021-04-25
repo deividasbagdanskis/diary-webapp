5b5cf43d38ea3fd51576d3a59e7e3d92e5df11eb
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-04-25 16:55:50 +0300

    Add all files

---
+++ diary/connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$link = mysqli_connect("localhost", "id16686090_root", "oZe~|9UcY#L7R42_", "id16686090_diary_users");
+
+if (mysqli_connect_error()) {
+    die("Database Connection Error");
+}
